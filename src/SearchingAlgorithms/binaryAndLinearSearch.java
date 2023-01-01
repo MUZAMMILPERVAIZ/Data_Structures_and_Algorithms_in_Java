1af1db93453966daa6784c3ac5e6ea767aa11b30
MUZAMMILPERVAIZ <muzammilpervaiz925139@gmail.com>
2023-01-01 21:17:41 +0500

    Commits new changes

--- src/SearchingAlgorithms/binaryAndLinearSearch.java
+++ src/SearchingAlgorithms/binaryAndLinearSearch.java
@@ -1,57 +1,71 @@
 package SearchingAlgorithms;
 
 public class binaryAndLinearSearch {
-    public static void main(String[] args) {
-        int [] array = {2,3,4,5,50,55,66,77};
-        int goal =55;
+    public static int steps = 0;
 
-        System.out.println("[ INFO ] Searching Linearly... ");
+    public static void main(String[] args) {
+        int[] array = {2, 3, 4, 5, 50, 55, 66, 77};
+        int goal = 77;
+        System.out.println("----------------------------------------");
+        System.out.println("[ INFO ] Linear Search... ");
         //TODO: LINEAR SEARCH
         for (int i = 0; i < array.length; i++) {
-            if(goal == array[i]){
-                System.out.println("[ INFO ] Found at index: "+i);
+            if (goal == array[i]) {
+
+                System.out.println("[ INFO ] Found at index: " + i);
             }
         }
 
 
-
-        //TODO: BINARY SEARCH
-        System.out.println("[ INFO ] Searching through Binary... ");
         int mid = findMid(array);
-        System.out.print("Mid Value OF Array Is "+ mid);
-        binarySearch(array,mid,goal);
+        System.out.print("Mid Value OF Array Is " + mid);
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println("----------------------------------------");
+        //TODO: BINARY SEARCH
+        System.out.println("\n[ INFO ] Binary Search... ");
+        binarySearch(array, mid, goal);
 
 
     }
 
-    public static void binarySearch(int arr[], int mid, int goal){
-        int lengthOne = mid+1;
-        int lenghtTwo =( arr.length -1 ) - (mid);
+    public static void binarySearch(int[] arr, int mid, int goal) {
+        steps++;
+        System.out.println("\n-------------------------- [ ARRAY ] ----------------------------:");
+        for (int element : arr) {
+            System.out.print(element + "  ");
+        }
+        int lengthOne = mid + 1;
+        int lenghtTwo = (arr.length - 1) - (mid);
         int a[] = new int[lengthOne];
         int b[] = new int[lenghtTwo];
 
-        if (mid == goal){
-            System.out.println("[ INFO ] Found at mid... ");
+        if (arr[mid] == goal) {
+            System.out.println("\n-------------------------- [ INFO ] ----------------------------:");
+            System.out.println(" Found at mid... ");
+            System.out.println(" Steps: " + steps);
             return;
-        }
-        else if(goal< arr[mid]){
+        } else if (goal < arr[mid]) {
             for (int i = 0; i < lengthOne; i++) {
                 a[i] = arr[i];
             }
             mid = findMid(a);
-            binarySearch(a,mid,goal);
-        } else if(goal > arr[mid]){
-            int c=0;
-            for (int i = mid; i < arr.length; i++) {
+            binarySearch(a, mid, goal);
+        } else if (goal > arr[mid]) {
+            int c = 0;
+            for (int i = mid + 1; i < arr.length; i++) {
                 b[c] = arr[i];
                 c++;
             }
             mid = findMid(b);
-            binarySearch(b,mid,goal);
+            binarySearch(b, mid, goal);
         }
     }
 
-    public static int findMid(int array[]){
+    public static int findMid(int array[]) {
         int low = 0;
         int high = array.length - 1;
         int mid = (low + high) >>> 1;
